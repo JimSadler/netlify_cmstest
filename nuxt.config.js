90089d5a3b07fd87ad7dd243b935a06d747dbd6b
Jim Sadler <jim@jsdevelopment.tech>
2021-06-13 12:58:56 -0400

    added generate in nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,6 +39,16 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: ["@nuxt/content"],
+  generate: {
+    async routes() {
+      const { $content } = require("@nuxt/content");
+      const files = await $content()
+        .only(["path"])
+        .fetch();
+
+      return files.map(file => (file.path === "/index" ? "/" : file.path));
+    }
+  },
   content: {
     // Options
   },
